f3606fdf933010d4e9385422d7196288bac2642d
jpb0816 <55253649+jpb0816@users.noreply.github.com>
2019-10-01 17:38:08 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centavos_a_pesos_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalcentavos, tpesos, tcentavos;
+            Console.WriteLine("Ingrese cantidad de centavos: ");
+            totalcentavos = Convert.ToInt32(Console.ReadLine());
+            Calcular(totalcentavos, out tpesos, out tcentavos);
+
+            Console.WriteLine("El resultado es: " + tpesos +" " +" Pesos " +"\t"+tcentavos+" "+" centavos ");
+
+
+
+             void Calcular(int tocentavos, out int pesos, out int centavos)
+            {
+                pesos = totalcentavos / 100;
+                centavos = totalcentavos % 100;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
